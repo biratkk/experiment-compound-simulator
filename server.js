0b05944a5485661878dbb3ddcd76750c7e078363
Birat Koirala <biratkk@amazon.com>
2026-02-01 18:00:22 +0000

    Added basic server to host html file

---
+++ server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    fs.readFile(path.join(__dirname, 'h2o.html'), (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Error loading file');
+            return;
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
